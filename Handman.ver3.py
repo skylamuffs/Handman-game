fec7c62769dbfea26b3fc786593df76a0dcdec6d
skylamuffs <59754812+skylamuffs@users.noreply.github.com>
2026-02-09 22:51:06 +1300

    1

--- Handman.ver3.py
+++ Handman.ver3.py
@@ -10,7 +10,7 @@ print("Hello,", name, "nice to meet you! Welcome to Hangman!!!!!!!!!!")
 
 print("\n" + "="*50)
 print("Here are the game rules:")
-print("1. I'll choose a random word for you to guess")
+print("1. There will be 10 guesses")
 print("2. You'll guess one letter at a time")
 print("3. Each incorrect guess brings you closer to losing")
 print("4. Guess the word before you run out of attempts!")
@@ -21,16 +21,11 @@ secret_word = random.choice(wordlist).upper()
 word_length = len(secret_word)
 
 
-# Game variables
+# Guesses
 guessed_letters = []
 incorrect_guesses = 0
-max_attempts = 6
+max_attempts = 10
 correctly_guessed = ['_'] * word_length
-
-
-print(f"\nAlright {name}, I've chosen a word!")
-print(f"The word has {word_length} letters.")
-print(f"You have {max_attempts} attempts to guess the word.\n")
 
 
 # Hangman stages (visual representation)
